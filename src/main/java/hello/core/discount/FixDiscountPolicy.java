613d29511a8e922f23e38652552b1d3e78b4e79e
이태균 <>
2022-10-17 15:28:38 +0900

    [이태균] Spring BASIC Spring 및 Bean 조회(Test Code)

--- src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -3,7 +3,7 @@ package hello.core.discount;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 
-public class FixdiscountPolicy implements DiscountPolicy {
+public class FixDiscountPolicy implements DiscountPolicy {
 
     private int discountFixAmount = 1000; // 1000원 할인
 
