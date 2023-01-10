e3d6b94668fa68807a984306cc48f8c2f18deab5
gyunny <needle12s@gmail.com>
2023-01-10 16:28:00 +0900

    feat : configuration

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -17,4 +17,9 @@ public class MemberServiceImpl implements MemberService {
     public Member findMember(Long memberId) {
         return memberRepository.findById(memberId);
     }
+
+    // 테스트
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
+    }
 }
