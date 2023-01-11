84a3f649f6833135e76301a8791a6ee24e914060
gyunny <needle12s@gmail.com>
2023-01-11 16:54:55 +0900

    wip

--- src/test/java/hello/core/discount/RateDiscountPolicyTest.java
+++ src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -34,7 +34,7 @@ class RateDiscountPolicyTest {
         int discount = discountPolicy.discount(member, 10000);
 
         // then
-        assertThat(discount).isEqualTo(1000);
+        assertThat(discount).isEqualTo(0);
     }
 
 }
