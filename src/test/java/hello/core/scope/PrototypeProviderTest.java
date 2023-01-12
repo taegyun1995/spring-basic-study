156906036e8a63643acd9c8461451a464c0d234a
gyunny <needle12s@gmail.com>
2023-01-12 13:22:34 +0900

    feat : prototype scope

---
+++ src/test/java/hello/core/scope/PrototypeProviderTest.java
@@ -0,0 +1,15 @@
+package hello.core.scope;
+
+import org.junit.jupiter.api.Test;
+
+public class PrototypeProviderTest {
+
+    @Test
+    void providerTest() {
+
+    }
+
+    static class ClientBean {
+        
+    }
+}
